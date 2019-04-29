fb1abffe9e9a5e23a3f074162ad3db94d7501f54
ljt521 <707482192@qq.com>
2019-04-29 19:12:06 +0800

    luyou

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <tabbar>
+      <tabbar-item selected link="/dynamic">
+        <!--<img slot="icon" src="./assets/img/ic_tab_home_normal.png">-->
+        <!--<img slot="icon-active" src="./assets/img/ic_tab_home_active.png">-->
+        <span slot="label">动态</span>
+      </tabbar-item>
+      <tabbar-item show-dot link="/customer">
+        <!--<img slot="icon" src="./assets/img/ic_tab_subject_normal.png">-->
+        <!--<img slot="icon-active" src="./assets/img/ic_tab_subject_active.png">-->
+        <span slot="label">客户</span>
+      </tabbar-item>
+      <tabbar-item badge="2" link="/mine">
+        <!--<img slot="icon" src="./assets/img/ic_tab_profile_normal.png">-->
+        <!--<img slot="icon-active" src="./assets/img/ic_tab_profile_active.png">-->
+        <span slot="label">我的</span>
+      </tabbar-item>
+    </tabbar>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { Tabbar, TabbarItem, XHeader } from 'vux'
+Vue.component('tabbar', Tabbar)
+Vue.component('tabbarItem', TabbarItem)
+Vue.component('x-header', XHeader)
+
 export default {
-  name: 'App'
+  name: 'APP',
+  data () {
+  }
 }
 </script>
 
@@ -17,6 +42,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/Mine.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <x-header>我的<a slot="right">保存</a></x-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Mine',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
