ba97645903453d68f077de90fce85d69f3f52a2d
ljt521 <707482192@qq.com>
2019-05-06 11:00:23 +0800

    添加组件

---
+++ .svn/pristine/ce/cedb4d25f153b5a679ade67bd0ac0e1a4b29cdd3.svn-base
@@ -0,0 +1,18 @@
+<template>
+    <div>
+     动态
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DynamicAssembly',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less">
+</style>
---
+++ src/components/FollowDetails.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+     跟进详情
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FollowDetails',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less">
+</style>
