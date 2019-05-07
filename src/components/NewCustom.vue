d0c8d06df6a5fd7d4636e7812d0e82c2d2de0679
ljt521 <707482192@qq.com>
2019-05-07 17:57:15 +0800

    添加组件

---
+++ src/components/NewCustom.vue
@@ -0,0 +1,20 @@
+<!--作者：莫岭红 功能：新建会诊量-->
+<template>
+<div>111</div>
+</template>
+<script>
+
+  export default {
+    name: 'NewCustom',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+
+
+</style>
