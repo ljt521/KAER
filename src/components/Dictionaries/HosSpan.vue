c4928b58c7f819710779fe3e4937862819560dc7
ljt521 <707482192@qq.com>
2019-06-12 17:06:27 +0800

    ss

---
+++ src/components/Dictionaries/HosSpan.vue
@@ -0,0 +1,32 @@
+<template>
+  <span>
+    {{message}}
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'HosSpan',
+    props: ['code'],
+    data() {
+      return{
+        message: {}
+      }
+    },
+    mounted() {
+      if(this.code){
+        this.$get('/api/hospital/' + this.code).then(info => {
+          this.message = info.hcoName
+        })
+      }
+
+    },
+    watch:{
+      dictItemCode(){
+        this.$get('/api/hospital/' + this.code).then(info => {
+          this.message = info.hcoName
+        })
+      }
+    }
+  }
+</script>
